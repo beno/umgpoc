288204c6e922e4daec347756272b671b5c20fc0d
Michel Benevento <michel@air.local>
2011-03-21 10:15:17 +0100

    call new table partials (for datatables)

--- app/helpers/parties_helper.rb
+++ app/helpers/parties_helper.rb
@@ -1,8 +1,8 @@
 module PartiesHelper
   
-  def list_remote_objects(party, association)
-    begin 
-      render :partial => "parties/#{association.to_s.singularize}", :collection => party.send(association)
+  def list_remote_objects(association)
+    begin
+      render :partial => "remote_resources/#{association.to_s}"
     rescue Exception => e
       "<span style='color:red'>Remote resource error: #{e}</span>".html_safe
     end
